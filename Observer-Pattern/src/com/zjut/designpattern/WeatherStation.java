4fb7058298eb91b6915b9dc9ccfbcc4b81ac6a8f
zjxjwxk <zjxjwxk@gmail.com>
2020-06-26 21:59:40 +0800

    add HeatIndexDisplay

--- Observer-Pattern/src/com/zjut/designpattern/WeatherStation.java
+++ Observer-Pattern/src/com/zjut/designpattern/WeatherStation.java
@@ -2,6 +2,7 @@ package com.zjut.designpattern;
 
 import com.zjut.designpattern.observer.CurrentConditionDisplay;
 import com.zjut.designpattern.observer.ForecastDisplay;
+import com.zjut.designpattern.observer.HeatIndexDisplay;
 import com.zjut.designpattern.observer.StatisticsDisplay;
 import com.zjut.designpattern.subject.WeatherData;
 
@@ -17,6 +18,7 @@ public class WeatherStation {
         CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay(weatherData);
         StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
         ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
